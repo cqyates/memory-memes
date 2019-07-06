b9bbefd2c87ca763e27b7ff05be8885d04314b07
Corey <cqyates@gmail.com>
2019-07-06 19:02:21 -0400

    Updates

---
+++ precache-manifest.3389aa5f082fdcda27802795fe273746.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7532408f5d2bc700e461d0efa0c26e40",
+    "url": "/memory-memes/index.html"
+  },
+  {
+    "revision": "646d1cf0a27629da9303",
+    "url": "/memory-memes/static/css/main.45c03ea1.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/memory-memes/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "646d1cf0a27629da9303",
+    "url": "/memory-memes/static/js/main.96f85d6a.chunk.js"
+  },
+  {
+    "revision": "6de73cd08f1c6d099a16",
+    "url": "/memory-memes/static/js/runtime~main.b99d8e3d.js"
+  }
+]);
